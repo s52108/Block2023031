46a041cbf197c28b936e0dcbff0efa8095d4e180
s52108 <s52108@fh.campus02.at>
2023-03-17 20:15:22 +0100

    Implementet ProductDemo & ProductManager

--- src/ProductDemo/ProductDemo.java
+++ src/ProductDemo/ProductDemo.java
@@ -6,6 +6,8 @@ public class ProductDemo {
 
     public static void main(String[] args) {
         String f = "C:\\Users\\postl\\OneDrive\\Dokumente\\Wirtschaftsinformatik\\2. Semester\\Erweiterte Themen der Softwareentwicklung\\sub-ordner\\dokumente\\ProductDemo.dat";
+        //String g = "C:\\Users\\postl\\OneDrive\\Dokumente\\Wirtschaftsinformatik\\2. Semester\\Erweiterte Themen der Softwareentwicklung\\sub-ordner\\dokumente\\ProductDemoLoad.dat";
+
         ProductManager asdf = new ProductManager();
         Product schnitzel = new Product("Wiener", 13.5, "Fleisch");
         Product doener = new Product("Döner", 3.5, "Fleisch");
@@ -13,6 +15,9 @@ public class ProductDemo {
         asdf.add(schnitzel);
         asdf.add(doener);
         asdf.save(f);
+
+        asdf.load(f);
+        asdf.print();
 
     }
 }
--- src/ProductDemo/ProductManager.java
+++ src/ProductDemo/ProductManager.java
@@ -20,6 +20,7 @@ public class ProductManager {
             for (Product p : product) {
                 oos.writeObject(p);
             }
+            oos.writeObject(null);
             oos.flush();
 
         } catch (FileNotFoundException e) {
@@ -32,5 +33,29 @@ public class ProductManager {
 
     public void load(String path) {
 
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
+
+            Object readObject;
+
+            while ((readObject = ois.readObject()) != null) {
+                Product p = (Product) readObject;
+                add(p);
+            }
+
+        } catch (EOFException eof) {
+            System.out.println("Ende erreicht - juhu!!! alles gelesen!!!");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void print() {
+        System.out.println("product = " + product);
     }
 }
